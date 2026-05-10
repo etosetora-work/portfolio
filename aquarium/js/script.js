3525e8d773fe9bf19a5221d100b5977952e10912
瀬虎えと <etosetora.work@gmail.com>
2026-05-10 19:13:11 +0900

    Add files via upload

--- aquarium/js/script.js
+++ aquarium/js/script.js
@@ -17,3 +17,33 @@
         pauseOnHover: false,//マウスホバーで一時停止を無効
         pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
 });
+
+//ハンバーガーメニュー
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  menu.classList.toggle('active');
+});
+
+
+//アコーディオン
+window.onload = init();
+
+function init() {
+  const accordion_items = document.querySelectorAll(".accordion_title");
+  for (var i = 0; i < accordion_items.length; i++) {
+    accordion_items[i].addEventListener("click", function () {
+      this.nextElementSibling.classList.toggle("show");
+      this.classList.toggle("active");
+      if (this.classList.contains("active")) {
+        this.nextElementSibling.style.height =
+          // 40は余白分
+          this.nextElementSibling.children[0].clientHeight + 20 + "px";
+      } else {
+        this.nextElementSibling.style.height = 0;
+      }
+    });
+  }
+}
